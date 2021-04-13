175ed6643abd05042d6de473f6c7f038ce64f1eb
Umutcan <umutcanceyhan@gmail.com>
2021-04-13 16:00:17 +0300

    Bootstrap added to layouts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="">
+                <button class="btn btn-secondary">
+                    Possessors
+                </button>
+                </div>
+                <div>
+                <button class="btn btn-primary">
+                    Pokemons
+                </button>
+                </div>
             </div>
         </div>
     </div>
